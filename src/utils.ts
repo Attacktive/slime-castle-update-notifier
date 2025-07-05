6b693660b09661ebfe7c548510df86885b27152d
Attacktive <tribute2pantera@gmail.com>
2025-07-05 11:36:59 +0900

    feature: parse iTunes API response to get version
    https://github.com/Attacktive/slime-castle-update-notifier/issues/69

--- src/utils.ts
+++ src/utils.ts
@@ -1,75 +1,79 @@
-import { chromium } from "@playwright/test";
+import axios from "axios";
 
-const URL_TO_POKE = "https://play.google.com/store/apps/details?id=com.redtailworks.slimetd";
-const retrieveVersion = async (): Promise<string> => {
-	const browser = await chromium.launch({ headless: true });
+interface Metadata {
+	screenshotUrls: string[];
+	appletvScreenshotUrls: string[];
+	ipadScreenshotUrls: string[];
+	artworkUrl512: string;
+	artworkUrl60: string;
+	artworkUrl100: string;
+	isGameCenterEnabled: boolean;
+	features: string[];
+	supportedDevices: string[];
+	advisories: string[];
+	kind: string;
+	artistViewUrl: string;
+	userRatingCountForCurrentVersion: number;
+	averageUserRatingForCurrentVersion: number;
+	fileSizeBytes: string;
+	formattedPrice: string;
+	trackContentRating: string;
+	sellerUrl: string;
+	languageCodesISO2A: string[];
+	artistId: number;
+	artistName: string;
+	genres: string[];
+	price: number;
+	primaryGenreName: string;
+	primaryGenreId: number;
+	bundleId: string;
+	releaseDate: string;
+	trackId: number;
+	trackName: string;
+	isVppDeviceBasedLicensingEnabled: boolean;
+	sellerName: string;
+	currentVersionReleaseDate: string;
+	releaseNotes: string;
+	version: string;
+	wrapperType: string;
+	currency: string;
+	description: string;
+	minimumOsVersion: string;
+	trackCensoredName: string;
+	trackViewUrl: string;
+	contentAdvisoryRating: string;
+	averageUserRating: number;
+	genreIds: string[];
+	userRatingCount: number;
+}
 
-	try {
-		const context = await browser.newContext();
-		const page = await context.newPage();
-		console.debug("navigating to the page...");
+interface ItunesResponse {
+	resultCount: number;
+	results: Metadata[];
+}
 
-		await page.setViewportSize({ width: 1080, height: 1920 });
-		await page.goto(URL_TO_POKE, { waitUntil: "networkidle", timeout: 30000 });
-		console.debug("page loaded", page);
+const APP_ID = 6480410032;
+const URL_TO_APP = `https://itunes.apple.com/lookup?id=${APP_ID}`;
 
-		await page.waitForLoadState("domcontentloaded");
-		console.debug("DOM content loaded");
+const retrieveVersion = async () => {
+	console.debug(`Trying to look up: ${URL_TO_APP}`);
 
-		const buttonSelector = "button[data-idom-class]";
-		console.debug("looking for the button", buttonSelector);
-		const button = page.locator(buttonSelector)
-			.filter({ hasText: "arrow_forward" })
-			.first();
+	const { data } = await axios.get<ItunesResponse>(URL_TO_APP);
+	const { results, resultCount } = data;
 
-		console.debug("found the button; going to click it", button);
-		await button.click();
-
-		const dialogSelector = "div[jsshadow] div[role=dialog]";
-		console.debug("waiting for the dialog", dialogSelector);
-		const dialog = await page.waitForSelector(dialogSelector, { state: "visible", timeout: 10000 });
-		if (!dialog) {
-			throw new Error(`The dialog (${dialogSelector}) not found.`);
-		}
-
-		console.debug("found the dialog", dialog);
-
-		// fixme
-		console.debug("unconditionally waiting for the animation to finish. 🤢🤮");
-		await page.waitForTimeout(2000);
-
-		const versionSelector = "div[jsslot][jsname] > div[jscontroller] > div[jsaction][jsshadow][jscontroller] > div[jsname][jsslot] > div:nth-child(3) > div > div:nth-child(2)";
-		console.debug(`looking for the version division`, versionSelector);
-		const versionDiv = await dialog.waitForSelector(versionSelector, { timeout: 10000 });
-		if (!versionDiv) {
-			throw new Error(`The division element (${dialogSelector}) not found.`);
-		}
-
-		console.debug("found the version division", versionDiv);
-
-		const versionDivText = await versionDiv.textContent();
-		if (!versionDivText) {
-			throw new Error(`The "textContent" is evaluated to ${versionDivText}.`);
-		}
-
-		console.debug(`the "textContent" of the version division: ${versionDivText}`);
-
-		const regExp = /v?(\d+(?:\.\d+(?:\.\d+)?)?)/i;
-		const match = regExp.exec(versionDivText);
-		if (!match) {
-			throw new Error(`${versionDivText} failed to pass the pattern: ${regExp}`);
-		}
-
-		console.debug(`found the match against ${regExp}`, match);
-
-		const [, version] = match;
-
-		console.debug("finally found the version", version);
-
-		return version;
-	} finally {
-		await browser.close();
+	if (resultCount === 0 || results.length === 0) {
+		throw new Error(`No result found for app ID: ${APP_ID}`);
 	}
+
+	const ids = results.map(({ trackId }) => trackId);
+	console.warn(`More than one result found!`, ids);
+	if (!ids.includes(APP_ID)) {
+		throw new Error(`No relevant result found for app ID: ${APP_ID}`);
+	}
+
+	const { version } = results.find(({ trackId }) => trackId === APP_ID)!;
+
+	return version;
 };
 
 export { retrieveVersion };
